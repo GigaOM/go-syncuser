451c7f43aac1e8f6107858ef8bfed9b393906d8a
Will Luo <will.luo@gigaom.com>
2014-08-20 12:53:06 -0700

    removed the unused 'go_syncuser_user_update_sync' admin ajax

--- components/class-go-syncuser-admin.php
+++ components/class-go-syncuser-admin.php
@@ -28,38 +28,11 @@ class GO_Sync_User_Admin
 	 */
 	public function admin_init()
 	{
-		add_action( 'wp_ajax_go_syncuser_user_update_sync', array( $this, 'user_update_sync_ajax' ) );
-
 		wp_register_script( 'go-usersync-admin-settings', plugins_url( '', __FILE__ ) . '/js/go-usersync-admin-settings.js', array( 'jquery' ), $this->core->version, TRUE );
 		wp_localize_script( 'go-usersync-admin-settings', 'go_usersync_ajax', array( 'admin_ajax_url' => admin_url( '/admin-ajax.php' ) ) );
 
 		wp_enqueue_script( 'go-usersync-admin-settings' );
 	}//END admin_init
-
-	/**
-	 * Ajax callback to sync a user immediately. The user id is passed in
-	 * via a query var 'go_syncuser_user_id'. We always invoke the 
-	 * user sync action with the action type 'update' when sync'ed by
-	 * this ajax function.
-	 */
-	public function user_update_sync_ajax()
-	{
-		// only allowed for people who can edit users
-		if ( ! current_user_can( 'edit_users' ) )
-		{
-			die;
-		}
-
-		if ( ! ( $user = $this->core->sanitize_user( wp_filter_nohtml_kses( $_REQUEST[ 'go_syncuser_user_id' ] ) ) ) )
-		{
-			echo '<p class="error">Missing user id</p>';
-			die;
-		}
-
-		do_action( 'go_syncuser_user', $user->ID, 'update' );
-
-		die;
-	}//END user_update_sync_ajax
 
 	/**
 	 * Add a menu item for the settings page
